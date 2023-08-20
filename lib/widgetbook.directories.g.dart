8f4448af01f4b3b26011c77fa02ef32c1d845abf
hannah <hannahnajwa@gmail.com>
2023-08-20 12:59:03 +0800

    Setup widgetbook

---
+++ lib/widgetbook.directories.g.dart
@@ -0,0 +1,29 @@
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_import, prefer_relative_imports, directives_ordering
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AppGenerator
+// **************************************************************************
+
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_demo/widgetbook.dart';
+
+final directories = <WidgetbookNode>[
+  WidgetbookFolder(
+    name: 'widgets',
+    children: [
+      WidgetbookComponent(
+        name: 'Container',
+        useCases: [
+          WidgetbookUseCase(
+            name: 'with green color',
+            builder: (context) => greenContainerUseCase(context),
+          ),
+        ],
+      ),
+    ],
+  ),
+];
